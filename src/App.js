9385eec5bbc9db53716da5376aaf67a4af05d622
admin <admin@admins-MacBook-Pro.local>
2019-10-13 13:49:51 -0700

    changed header

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello my name is Kevin Nguyen
         </a>
       </header>
     </div>
