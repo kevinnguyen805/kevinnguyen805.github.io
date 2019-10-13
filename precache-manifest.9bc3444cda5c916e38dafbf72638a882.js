e9181d58658917d8ac4dcc54e62701afe2c9444d
admin <admin@admins-MacBook-Pro.local>
2019-10-13 13:39:39 -0700

    Updates

---
+++ precache-manifest.9bc3444cda5c916e38dafbf72638a882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb200458f2595e523575302db8bef95f",
+    "url": "/portfoliosite/index.html"
+  },
+  {
+    "revision": "bda7694c6306bc29b9c5",
+    "url": "/portfoliosite/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8c22202281c0eedd565b",
+    "url": "/portfoliosite/static/js/2.59c499dc.chunk.js"
+  },
+  {
+    "revision": "bda7694c6306bc29b9c5",
+    "url": "/portfoliosite/static/js/main.fbcc6200.chunk.js"
+  },
+  {
+    "revision": "15e355a8d3158863c2ba",
+    "url": "/portfoliosite/static/js/runtime-main.0343731e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfoliosite/static/media/logo.25bf045c.svg"
+  }
+]);
